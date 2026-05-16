b949f1be0dfec368dc5a1c2f669b18d783103088
Mukhlis Maulana Al-Fakih <mukhlis.maulanaalf@gmail.com>
2026-05-17 02:31:27 +0700

    update: change id to name of data for user management

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,7 +2,11 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use App\Models\Location;
+use App\Models\Schedule;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Facades\Hash;
@@ -25,15 +29,20 @@ class UserForm
                 DateTimePicker::make('email_verified_at'),
                 TextInput::make('password')
                     ->password()
-                    ->required()
+                    ->default('tkj@123456')
+                    ->hiddenOn('edit')
                     ->dehydrateStateUsing(fn ($state) => Hash::make($state)),
                 TextInput::make('role')
                     ->required()
                     ->default('employee'),
-                TextInput::make('location_id')
-                    ->numeric(),
-                TextInput::make('schedule_id')
-                    ->numeric(),
+                Select::make('location_id')
+                    ->label('Assign Location')
+                    ->options(Location::pluck('name', 'id'))
+                    ->searchable(),
+                Select::make('schedule_id')
+                    ->label('Schedule')
+                    ->options(Schedule::pluck('name', 'id'))
+                    ->searchable(),
                 TextInput::make('device_id'),
             ]);
     }
